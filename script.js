a2c9b994ba3c7c74315d21f448d097c261d68f54
alvarojimeenez <ajp51099@gmail.com>
2023-09-22 13:01:33 +0200

    Last update

--- script.js
+++ script.js
@@ -10,21 +10,23 @@ y horarios nos la introduzca en la tabla añadiendo una fila siempre al final co
 agregar.addEventListener('click',()=> {
     let fila = document.createElement('tr');
     lista.appendChild(fila);
-    if (equipo1.value != equipo2.value) {
+    if ((equipo1.value.trim().toLowerCase() === equipo2.value.trim().toLowerCase()) || (equipo1.value == "" || equipo2.value == "" || horario.value == "")) {
+        alert("No puedes agregar en un mismo partido al mismo equipo o faltan campos por rellenar.");
+    }else {
         fila.insertAdjacentHTML('beforeend',`<td>${equipo1.value}</td>`);
         fila.insertAdjacentHTML('beforeend',`<td>${equipo2.value}</td>`);
         fila.insertAdjacentHTML('beforeend',`<td>${horario.value}</td>`);
-    }else {
-        alert("No puedes agregar en un mismo partido al mismo equipo");
     }
 
 })
 /* En la variable del boton de borrar equipo; al hacer clic en ella borra siempre la ultima fila de la tabla */
 borrar.addEventListener('click', ()=> {
-    let fila = lista.getElementsByTagName('tr').length;
-    if (fila>1) {
-        lista.getElementsByTagName('tr')[fila-1].remove();
-    }else {
+    let fila = lista.getElementsByTagName('tr');
+    let posicion = parseInt(prompt("Seleccione fila a borrar: "));
+    if (posicion<=0) {
         alert("No hay más partidos para borrar");
+    }else {
+        fila[posicion].remove();
     }
 })
+
